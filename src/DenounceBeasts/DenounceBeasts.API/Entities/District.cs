4f6dfc59b3ca1b3c2e827e3e513e16af5459a4d2
Jobell599 <jobeldicley123@gmail.com>
2025-07-29 10:45:06 -0400

    summit

--- src/DenounceBeasts/DenounceBeasts.API/Entities/District.cs
+++ src/DenounceBeasts/DenounceBeasts.API/Entities/District.cs
@@ -2,5 +2,21 @@
 {
     public class District
     {
+        public int Id { get; set; }
+
+        public DateTime CreateAt { get; set; }
+
+        public DateTime? UpdateAt { get; set; }
+
+        public bool IsActive { get; set; }
+        public string Name { get; set; }
+
+        public string Code { get; set; }
+
+        public int MunicipalityId { get; set; }
+
+        public virtual Municipality Municipality { get; set; }
+
+        //public virtual ICollection<Complain> Complaints { get; set; }
     }
 }
--- src/DenounceBeasts/DenounceBeasts.API/Entities/Municipality.cs
+++ src/DenounceBeasts/DenounceBeasts.API/Entities/Municipality.cs
@@ -1,6 +1,20 @@
-﻿namespace DenounceBeasts.API.Entities
+﻿using System.Data;
+
+namespace DenounceBeasts.API.Entities
 {
     public class Municipality
     {
+        public int Id { get; set; }
+
+        public DateTime CreateAt {  get; set; }
+
+        public DateTime? UpdatedAt { get; set; }
+
+        public bool IsActive { get; set; }
+        public string Name { get; set; }
+
+        public string Code { get; set; }
+
+        public virtual ICollection<District> Districts { get; set; } = new List<District>();
     }
 }
